a8d99fcc4b85f11c92edc2ed946764878c58fe84
huseyincy0 <huseyincayir00@gmail.com>
2025-06-23 17:12:07 +0300

    ikinci commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,53 +1,55 @@
 import 'package:flutter/material.dart';
 
-const startAliment = Alignment.topLeft;
-const endAliment = Alignment.bottomRight;
+import 'dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
-  
-  var activeDiceImage = 'assets/images/dice-1.png';
-
-  void roleDice() {
-    activeDiceImage = 'assets/images/dice-2.png';
-    print("Dice Rolled");
-  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [color1, color2],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/images/dice-1.png', width: 200, 
-            ),
-            TextButton(
-              onPressed: 
-                () {
-                roleDice();
-                },
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                 textStyle: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-              ),
-            ), child: const Text('Roll Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World!'),
+//       ),
+//     );
+//   }
+// }
